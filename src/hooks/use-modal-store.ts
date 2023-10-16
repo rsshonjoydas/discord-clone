bb1de008cf18fdce0a890d11fb2c038f12719182
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-10-17 05:37:23 +0600

    feat: invite

--- src/hooks/use-modal-store.ts
+++ src/hooks/use-modal-store.ts
@@ -1,18 +1,25 @@
 /* eslint-disable no-unused-vars */
+import { Server } from '@prisma/client';
 import { create } from 'zustand';
 
-export type ModalType = 'createServer';
+export type ModalType = 'createServer' | 'invite';
+
+interface ModalData {
+  server?: Server;
+}
 
 interface ModalStore {
   type: ModalType | null; // Correct the type declaration
+  data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType) => void; // Correct the parameter type
+  onOpen: (type: ModalType, data?: ModalData) => void; // Correct the parameter type
   onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
+  data: {},
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type }), // Specify the parameter type
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }), // Specify the parameter type
   onClose: () => set({ type: null, isOpen: false }),
 }));
