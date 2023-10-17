880ddc2a92895f6a0aaaa5bae62905dfdd8061d9
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-10-17 15:14:20 +0600

    feat: members

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -54,7 +54,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
               Server Settings
               <Settings className='h-4 w-4 ml-auto' />
             </DropdownMenuItem>
-            <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+            <DropdownMenuItem
+              onClick={() => onOpen('members', { server })}
+              className='px-3 py-2 text-sm cursor-pointer'
+            >
               Manage Members
               <Users className='h-4 w-4 ml-auto' />
             </DropdownMenuItem>
