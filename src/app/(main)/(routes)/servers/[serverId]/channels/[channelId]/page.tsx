cd6ba67f66be3d6f8cba87d9f33e6fade5748f38
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-10-28 10:40:56 +0600

    feat: audio and video call

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,11 @@
 import { redirectToSignIn } from '@clerk/nextjs';
+import { ChannelType } from '@prisma/client';
 import { redirect } from 'next/navigation';
 
 import { ChatHeader } from '@/components/chat/chat-header';
 import { ChatInput } from '@/components/chat/chat-input';
 import { ChatMessages } from '@/components/chat/chat-messages';
+import { MediaRoom } from '@/components/media-room';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 
@@ -41,29 +43,35 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
       <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
-      <ChatMessages
-        member={member}
-        name={channel.name}
-        chatId={channel.id}
-        type='channel'
-        apiUrl='/api/messages'
-        socketUrl='/api/socket/messages'
-        socketQuery={{
-          channelId: channel.id,
-          serverId: channel.serverId,
-        }}
-        paramKey='channelId'
-        paramValue={channel.id}
-      />
-      <ChatInput
-        name={channel.name}
-        type='channel'
-        apiUrl='/api/socket/messages'
-        query={{
-          channelId: channel.id,
-          serverId: channel.serverId,
-        }}
-      />
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatMessages
+            member={member}
+            name={channel.name}
+            chatId={channel.id}
+            type='channel'
+            apiUrl='/api/messages'
+            socketUrl='/api/socket/messages'
+            socketQuery={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+            paramKey='channelId'
+            paramValue={channel.id}
+          />
+          <ChatInput
+            name={channel.name}
+            type='channel'
+            apiUrl='/api/socket/messages'
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
+      {channel.type === ChannelType.AUDIO && <MediaRoom chatId={channel.id} video={false} audio />}
+      {channel.type === ChannelType.VIDEO && <MediaRoom chatId={channel.id} video audio />}
     </div>
   );
 };
--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import { Hash } from 'lucide-react';
 import { MobileToggle } from '@/components/mobile-toggle';
 import { SocketIndicator } from '@/components/socket-indicator';
 import { UserAvatar } from '@/components/user-avatar';
+import { ChatVideoButton } from './chat-video-button';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -20,6 +21,7 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
     )}
     <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
     <div className='ml-auto flex items-center'>
+      {type === 'conversation' && <ChatVideoButton />}
       <SocketIndicator />
     </div>
   </div>
